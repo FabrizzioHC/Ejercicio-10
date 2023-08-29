6c6d4d4c733c42734a597067b7a820a72b5c3e7e
FABRIZZIO <FABRIZZIO@DESKTOP-8PLVDLM>
2023-08-28 21:04:50 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio 10/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double consumo, descuento, impuesto, subtotal, total;
+
+            Console.WriteLine("ingresa el monto del consumo:");
+            consumo = double.Parse(Console.ReadLine());
+
+            if (consumo <= 100)
+            {
+                descuento = consumo * 0.1;
+            }
+            else
+            {
+                descuento = consumo * 0.2;
+            }
+
+            impuesto = consumo * 0.18;
+            subtotal = consumo - descuento;
+            total = subtotal + impuesto;
+
+            Console.WriteLine($"monto del descuento: {descuento:C}");
+            Console.WriteLine($"impuesto: {impuesto:C}");
+            Console.WriteLine($"subtotal: {subtotal:C}");
+            Console.WriteLine($"importe a pagar: {total:C}");
+            Console.WriteLine("presiona cualquier tecla pa salir");
+            Console.ReadKey();
+        }
+    }
+}
